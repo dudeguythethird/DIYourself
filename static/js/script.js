cfcc801e2f175cfa2566b8798bd11095e7403c9d
dudeguythethird <keablejoseph@gmail.com>
2021-03-09 14:54:59 +0000

    adds password validation javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+
+
+// The following password validation method was found: https://codepen.io/diegoleme/pen/surIK
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("password_confirm");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
