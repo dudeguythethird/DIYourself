c6293534f81fece7cdcc85669aa261233ab32bfd
dudeguythethird <keablejoseph@gmail.com>
2021-03-10 15:48:06 +0000

    add logout functionality and dynamic nav bar links

--- app.py
+++ app.py
@@ -85,7 +85,19 @@ def profile(username):
     # grab the session user's username from the DB, for display on the page.
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove the user from the session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
