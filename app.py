06c4619e1a4577b0765dfc7dbcd26d56bd0d6413
dudeguythethird <keablejoseph@gmail.com>
2021-03-10 13:21:03 +0000

    adds sign up functionality, including password verification check

--- app.py
+++ app.py
@@ -27,6 +27,27 @@ def get_methods():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        # check if username already exists in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already in use")
+            return redirect(url_for("sign_up"))
+
+        if request.form.get("password") == request.form.get("password_confirm"):
+            sign_up = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(
+                    request.form.get("password"))
+            }
+            mongo.db.users.insert_one(sign_up)
+
+            # put the new user into session cookie
+            session["user"] = request.form.get("username").lower()
+            flash("Sign-Up Successful!")
+
     return render_template("sign_up.html")
 
 
