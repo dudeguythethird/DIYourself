c47de48a22d6f4389d41c753524caa833e22714a
dudeguythethird <keablejoseph@gmail.com>
2021-03-11 14:52:08 +0000

    create very basic add method template

--- app.py
+++ app.py
@@ -100,6 +100,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_method")
+def add_method():
+    return render_template("add_method.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
